5d2b8599441d66c7e6c698c187b335fe97476b74
Chaudhari, Deepak <deepakkumar_chaudhari@intuit.com>
2020-11-25 12:41:45 -0800

    first commit

---
+++ src/com/codingpatterns/SlidingWindow/MinSizeSubArraySum.java
@@ -0,0 +1,59 @@
+package com.codingpatterns.SlidingWindow;
+
+
+/**
+ * Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+ *
+ * Example 1:
+ *
+ * Input: [2, 1, 5, 2, 3, 2], S=7
+ * Output: 2
+ * Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+ * Example 2:
+ *
+ * Input: [2, 1, 5, 2, 8], S=7
+ * Output: 1
+ * Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+ */
+
+public class MinSizeSubArraySum {
+
+    public static void main(String[] args) {
+        int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubArraySum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+        System.out.println("Smallest subarray length: " + result);
+    }
+
+
+    public static int findMinSubArray(int S, int[] inputArray) {
+        int maxSum = 0;
+        int windowSum = 0;
+        int windowStart = 0;
+
+        for (int windowEnd = 0; windowEnd < inputArray.length; windowEnd++) {
+
+            if(inputArray[windowEnd]>=S){
+                return 1;
+            }
+            else{
+                windowSum = windowSum + inputArray[windowEnd]; // add the next element
+                for(int subArraySize=2; windowEnd >= subArraySize - 1; subArraySize++){
+                    if(windowSum>=S){
+                        return subArraySize;
+                    }
+                    windowSum -= inputArray[windowStart]; // subtract the element going out
+                    windowStart++; // slide the window ahead
+
+                }
+
+            }
+
+        }
+        return maxSum;
+    }
+
+}
+
---
+++ src/com/codingpatters/recursion/LetterCasePermutation.java
@@ -0,0 +1,100 @@
+package com.codingpatters.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+ *
+ * Return a list of all possible strings we could create. You can return the output in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: S = "a1b2"
+ * Output: ["a1b2","a1B2","A1b2","A1B2"]
+ * Example 2:
+ *
+ * Input: S = "3z4"
+ * Output: ["3z4","3Z4"]
+ * Example 3:
+ *
+ * Input: S = "12345"
+ * Output: ["12345"]
+ * Example 4:
+ *
+ * Input: S = "0"
+ * Output: ["0"]
+ *
+ *
+ * Constraints:
+ *
+ * S will be a string with length between 1 and 12.
+ * S will consist only of letters or digits.
+ */
+
+public class LetterCasePermutation {
+
+    public static void main(String[] args) {
+
+        //PostitiveTestCase 1
+        String postitiveTestCase = "a1b2";
+        List<String> result = LetterCasePermutation.findInputPermutation(postitiveTestCase);
+        System.out.println("LetterCasePermutation: " + result.toString());
+
+
+        //PostitiveTestCase 2
+        String postitiveTestCase1 = "3z4";
+        List<String> result1 = LetterCasePermutation.findInputPermutation(postitiveTestCase1);
+        System.out.println("LetterCasePermutation: " + result1.toString());
+
+        //Only DigitCase
+        String onlyDigitCase = "434345";
+        List<String> result2 = LetterCasePermutation.findInputPermutation(onlyDigitCase);
+        System.out.println("LetterCasePermutation: " + result2.toString());
+
+        //Only DigitCase
+        String same = "ABCD";
+        List<String> result3 = LetterCasePermutation.findInputPermutation(same);
+        System.out.println("LetterCasePermutation: " + result3.toString());
+
+        //Only DigitCase
+        String sameLower = "ABCD";
+        List<String> result4 = LetterCasePermutation.findInputPermutation(sameLower);
+        System.out.println("LetterCasePermutation: " + result4.toString());
+    }
+
+    private static List<String> findInputPermutation(String inputString) {
+
+        List<String> result = new  ArrayList<>();
+
+        helper(inputString, 0, "", result);
+
+        return result;
+    }
+
+    private static void helper(String inputString, int idx, String soFar, List<String> coll){
+
+        if(idx==inputString.length()){
+            coll.add(soFar);
+        }else{
+            char currChar = inputString.charAt(idx);
+
+            if(Character.isDigit(currChar)){
+
+                helper(inputString, idx+1, soFar+currChar, coll);
+
+            }
+            else {
+                helper(inputString, idx+1, soFar + Character.toLowerCase(currChar), coll);
+                helper(inputString, idx+1, soFar + Character.toUpperCase(currChar), coll);
+            }
+
+        }
+
+
+    }
+
+
+}
